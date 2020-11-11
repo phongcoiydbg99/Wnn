f5c2e57ee6bd4c4c9931b7dfde81c190105e3aff
phongnve <phongnve@bkav.com>
2020-11-11 17:33:36 +0700

    first

---
+++ OpenWnn.1.1/app/build/generated/source/buildConfig/debug/jp/co/omronsoft/openwnn/BuildConfig.java
@@ -0,0 +1,10 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package jp.co.omronsoft.openwnn;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String APPLICATION_ID = "jp.co.omronsoft.openwnn";
+  public static final String BUILD_TYPE = "debug";
+}
